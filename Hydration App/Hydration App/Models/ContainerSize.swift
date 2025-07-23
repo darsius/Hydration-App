0e07ee78a67f74e38162051f7006b1b0908f3110
darsius <p_darius211@yahoo.com>
2025-07-23 11:22:44 +0300

    T01: Add TabView with Today's progress and History plain tabs

---
+++ Hydration App/Hydration App/Models/ContainerSize.swift
@@ -0,0 +1,8 @@
+//
+//  ContainerSize.swift
+//  Hydration App
+//
+//  Created by Paraschiv, Darius on 23.07.2025.
+//
+
+import Foundation
---
+++ Hydration App/Hydration App/Models/HydrationEntry.swift
@@ -0,0 +1,8 @@
+//
+//  HydrationEntry.swift
+//  Hydration App
+//
+//  Created by Paraschiv, Darius on 23.07.2025.
+//
+
+import Foundation
