8d94f71caf9fa00801cb5e22230d1b5b27df659f
darsius <p_darius211@yahoo.com>
2025-07-24 17:31:05 +0300

    refactor views to use constants for layout

---
+++ Hydration App/Hydration App/Constants/TodayConstants.swift
@@ -0,0 +1,17 @@
+//
+//  TodayViewConstants.swift
+//  Hydration App
+//
+//  Created by Paraschiv, Darius on 24.07.2025.
+//
+
+import SwiftUI
+
+enum TodayConstants {
+    static let dividerHeight: CGFloat = 1.5
+    static let topPadding: CGFloat = 40
+    static let currentGlassVolume: CGFloat = 20
+    static let containerSpacing: CGFloat = 8
+    static let containerVerticalPadding: CGFloat = 28
+    static let motivationalTextHorizontalPadding: CGFloat = 100
+}
