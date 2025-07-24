3f7bbea30616102c90a28134711d7bdf37da6bf9
darsius <p_darius211@yahoo.com>
2025-07-24 12:22:55 +0300

    Fix: replace tab bar items icons to use images form assets file

--- Hydration App/Hydration App/ContentView.swift
+++ Hydration App/Hydration App/ContentView.swift
@@ -2,19 +2,25 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    @State private var currentTab = 0
+    
     var body: some View {
-        TabView {
+        TabView(selection: $currentTab) {
             TodayView()
                 .tabItem {
-                    Image(systemName: "rectangle.portrait")
+                    Image(currentTab == 0 ? "Today_green" : "Today")
                     Text("Today")
                 }
+                .tag(0)
+            
             HistoryView()
                 .tabItem {
                     Image(systemName: "clock.arrow.trianglehead.counterclockwise.rotate.90")
                     Text("History")
                 }
+                .tag(1)
         }
+        .tint(.GREEN)
     }
 }
 
