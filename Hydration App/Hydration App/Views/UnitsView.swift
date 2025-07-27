ef12985f4856656d9acbcce0cb688ed7bd245295
darsius <p_darius211@yahoo.com>
2025-07-27 18:32:50 +0300

    T05: add all ui elements besides checkmark

--- Hydration App/Hydration App/Views/UnitsView.swift
+++ Hydration App/Hydration App/Views/UnitsView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct UnitsView: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text("Units")
-        Color.red.ignoresSafeArea()
+        VStack {
+            CustomDivderView()
+            List {
+                Section("") {
+                    HStack {
+                        Text("mililiters (ml)")
+                    }
+                    HStack {
+                        Text("ounces (oz)")
+                    }
+                }
+                .listRowSeparatorTint(Color(.WHITE))
+                .listRowBackground(Color(.GRAY_1))
+                .alignmentGuide(.listRowSeparatorLeading) { _ in
+                    return -20
+                }
+            }
+            .listStyle(.inset)
+            .navigationBarTitle("Units", displayMode: .inline)
+            .navigationBarBackButtonHidden(true)
+            .toolbar{
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Image(.back)
+                    }
+                }
+                
+            }
+        }
     }
 }
 
