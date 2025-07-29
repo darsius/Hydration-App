b30809d80b3fce2b1e460e812818ab92daa015ec
darsius <p_darius211@yahoo.com>
2025-07-29 09:42:01 +0300

    fix keyboard pop up zoom bug

---
+++ Hydration App/Hydration App/Constants/HydrationInputConstants.swift
@@ -0,0 +1,18 @@
+//
+//  HydrationImputConstants.swift
+//  Hydration App
+//
+//  Created by Paraschiv, Darius on 29.07.2025.
+//
+
+import Foundation
+
+enum HydrationInputConstants {
+    static let informationalTextTopPadding: CGFloat = 60
+    static let informationalTextHorizontalPadding: CGFloat = 100
+    static let textFieldHeight: CGFloat = 70
+    static let textFieldWidth: CGFloat = 120
+    static let textFieldCornerRadius: CGFloat = 12
+    static let textFieldBorderWidth: CGFloat = 3
+    static let textBottomPadding: CGFloat = 180
+}
