ae007e87d018baabe53b6b257de254de2cfd7da1
darsius <p_darius211@yahoo.com>
2025-08-13 10:19:01 +0300

    refactor: use view model unit and inputValue instead of bindings

--- Hydration App/Hydration App/Features/Input/HydrationInputViewModel.swift
+++ Hydration App/Hydration App/Features/Input/HydrationInputViewModel.swift
@@ -11,16 +11,18 @@ class HydrationInputViewModel: ObservableObject {
     private let userDefaultsKey: String
     private let notificationName: Notification.Name
     
-    @Published var value: Int
+    @Published var initialValue: Int
+    @Published var unit: UnitType
     
-    init(initialValue: Int, userDefaultsKey: String, notificationName: Notification.Name) {
-        self.value = initialValue
+    init(initialValue: Int, userDefaultsKey: String, notificationName: Notification.Name, unit: UnitType) {
+        self.initialValue = initialValue
         self.userDefaultsKey = userDefaultsKey
         self.notificationName = notificationName
+        self.unit = unit
     }
     
     func saveValue(_ newValue: Int) {
-        value = newValue
+        initialValue = newValue
         UserDefaults.standard.set(newValue, forKey: userDefaultsKey)
         NotificationCenter.default.post(name: notificationName, object: nil)
     }
